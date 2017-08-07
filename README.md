2bd007609232c42b80d10331582adadf81da4157
Bernard <g2pc@visionduweb.com>
2017-08-08 00:25:45 +0200

    Mise à jour par upload
    Mise à jour de la présentation.
    Mise à jour de la liste.
    Ajout d'une partie scam.
    Ajout d'un dossier readme qui ne sera pas installé, qui contient une image, avec des mots clés sur la blockchain.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mod_bitcoinfaucet
 Module Bitcoin Faucet pour Joomla 3.x
+
+Création d'un portefeuille avec block.io
+
+Jouer et gagner des Bitcoins.
+
+Faire un don au développeur.
--- tmpl/default.php
+++ tmpl/default.php
@@ -1,11 +1,6 @@
 <?php 
 defined('_JEXEC') or die; ?>
-<!--
-No direct access
-Vérifier si l'installation se passe bien avec UTF-8.
-Le copié collé manuel vers default.php depuis le FTP en ligne de LWS ne fonctionne pas avec UTF-8.
-Charger le fichier totalement en cas de modification fonctionne et permet d'afficher les caractères avec accents normalement.
--->
+<!-- No direct access -->
 
 <script language="JavaScript">
 function ChangeUrlBitcoin(formbitcoin)
@@ -14,7 +9,7 @@ function ChangeUrlBitcoin(formbitcoin)
       {
      var urlbitcoin ;
      urlbitcoin = formbitcoin.bitcoin.options[formbitcoin.bitcoin.selectedIndex].value;
-     window.open(urlbitcoin,'_blank','toolbar=0, location=0, directories=0, status=0, scrollbars=0, resizable=0, copyhistory=0, menuBar=0');
+     window.open(urlbitcoin,'_blank','toolbar=1, location=0, directories=1, status=1, scrollbars=1, resizable=1, copyhistory=1, menuBar=1');
        }
    }
 </script>
@@ -26,23 +21,28 @@ function ChangeUrlBitcoin(formbitcoin)
 <span>
 <form method="get" name="formbitcoin">
 <select name="bitcoin" id="bitcoin" size=1 onchange="ChangeUrlBitcoin(this.form)">
-<option selected value="">-Gagner des Bitcoins-</option>
-  <option value="https://block.io" title="Créer un portefeuille.&#013;Stocker des Bitcoins.&#013;Gratuit et rapide.">1 ★ ★ ★ Créer un portefeuille</option>
-  <option value="https://www.bitcoinget.com/?r=3GJ1uxp29CPgfU3jqzPJQAAqjL2RNJkfZz" title="Répondre à des sondages.&#013;Installer des jeux.&#013;Installer des applications.">2 ★ ➘ ➙ Bitcoinget.com</option>
-  <option value="https://bitminer.io/2345991" title="Gains de Satoshis permanent.&#013;Vérifier si le service transfert les Satoshis.&#013;Vérifier si des Satoshis sont transférés pour de nouvelles affiliations.">2 ❄ ❄ ➙ Bitminer.io</option>
-  <option value="https://bitzonk.com?afid=3662" title="Gains de 500 Satoshis toutes les 30 minutes.&#013;Limite à 1500 Satoshis offerts en tout et pour tout.&#013;Plateforme de jeux qui nécessite un dépôt de Satoshis.">2 ➘ ➘ ➘ Bitzonk.com</option>
-  <option value="https://bonusbitcoin.co/?ref=D33CF2C73520" title="Gains de Satoshis toutes les 15 minutes.&#013;Répondre à des sondages.&#013;Tester des logiciels.">2 ★ ★ ★ Bonusbitcoin.co</option>
-  <option value="https://btcprominer.life/228462" title="Gains de Satoshis permament.&#013;Vérifier si le service transfert les Satoshis.&#013;Des Satoshis sont transférés pour de nouvelles affiliations valides.">2 ☢ ★ ➙ Btcprominer.life</option>
-  <option value="https://dailyfreebits.com/?id=45822551" title="Gains de 30 Satoshis toutes les 60 minutes.&#013;Le montant offert est vraiment minime.">2 ★ ★ ➙ Dailyfreebits.com</option>
-  <option value="https://faucethub.io/r/7442367" title="Plateforme qui liste les meilleurs services pour gagner des Bitcoins.&#013;S'inscrire et récolter sur les sites présentés.">2 ★ ★ ★ Faucethub.io</option>
-  <option value="https://freebitco.in/?r=4223431" title="Gains de Satoshis toutes les 60 minutes.">2 ★ ★ ★ Freebitco.in</option>
-  <option value="http://frogfaucet.com/?r=100519" title="Gains de Satoshis toutes les 10 minutes.">2 ★ ★ ★ Frogfaucet.com</option>
-  <option value="http://getcoin.site/bitcoin/?id=1616" title="Gains de quelques Satoshis.&#013;Inscription avec votre portefeuille Bitcoin, votre portefeuille doit également être lié sur Faucethub.">2 ★ ➙ ➙ Getcoin.site/bitcoin</option>
-  <option value="https://hashrev.com/?r=108695">2 Hashrev.com</option>
-  <option value="https://mellowads.com/?ref=346FD8CD381D" title="Gains de Satoshis toutes les 24 heures&#013;Plateforme pour réinvestir dans la publicité.">2 ★ ★ ★ Mellowads.com</option>
-  <option value="https://moonbit.co.in/?ref=a4e136b6aac0" title="Gains de Satoshis permanent.">2 ★ ★ ★ Moonbit.co.in</option>
-  <option value="https://xtenfaucet.com/?r=34xh4B35bYqtWGUnxYeGTMJL7bwgrdw9Pp" title="Gains de Satoshis toutes les 30 minutes.&#013;Il faut ajouter son compte Bitcoin sur FaucetHub.&#013;Les transferts ne semblent pas se faire.">2 ❄ ❄ ❄ Xtenfaucet.com</option>
-  <option value="https://www.visionduweb.eu/altcoins/bitcoins/qrcode/" title="Faire un don en Bitcoins pour Vision du web.">3 ★ ★ ★ Un don pour Vision du web</option>
+<option selected value="#">--- Gagner des Bitcoins ---</option>
+ <option value="#" title="Créer un portefeuille.">- Créer un portefeuille -</option>
+ <option value="https://block.io" title="Conserver vos Bitcoins en ligne.&#013;Gratuit et rapide.">Block.io</option>
+ <option value="#" title="Bitcoins gratuits.">- Le top du Bitcoin gratuit -</option>
+ <option value="https://www.bitcoinget.com/?r=3GJ1uxp29CPgfU3jqzPJQAAqjL2RNJkfZz" title="Gagner des Satoshis.&#013;Répondre à des sondages.&#013;Installer des applications.&#013;Installer des jeux.">★ ➘ ➙ Bitcoinget.com</option>
+ <option value="https://bonusbitcoin.co/?ref=D33CF2C73520" title="Gains de Satoshis toutes les 15 minutes.&#013;Répondre à des sondages.&#013;Tester des logiciels.">★ ★ ★ Bonusbitcoin.co</option>
+ <option value="https://dailyfreebits.com/?id=45822551" title="Gains de 30 Satoshis toutes les 60 minutes.&#013;Le montant offert est minime.">★ ★ ➙ Dailyfreebits.com</option>
+ <option value="https://faucethub.io/r/7442367" title="Plateforme qui liste les meilleurs services pour gagner des Bitcoins.&#013;S'inscrire et récolter sur les sites présentés.">★ ★ ★ Faucethub.io</option>
+ <option value="https://freebitco.in/?r=4223431" title="Gains de Satoshis toutes les 60 minutes.">★ ★ ★ Freebitco.in</option>
+ <option value="http://frogfaucet.com/?r=100519" title="Gains de Satoshis toutes les 10 minutes.">★ ★ ★ Frogfaucet.com</option>
+ <option value="http://getcoin.site/bitcoin/?id=1616" title="Gains de quelques Satoshis.&#013;Inscription avec votre portefeuille Bitcoin.&#013;Votre portefeuille doit être lié sur Faucethub.">★ ➙ ➙ Getcoin.site/bitcoin</option>
+ <option value="https://mellowads.com/?ref=346FD8CD381D" title="Gains de Satoshis toutes les 24 heures&#013;Plateforme pour réinvestir dans la publicité.">★ ★ ★ Mellowads.com</option>
+ <option value="https://moonbit.co.in/?ref=a4e136b6aac0" title="Gains de Satoshis permanent.">★ ★ ★ Moonbit.co.in</option>
+ <option value="http://onewayfaucet.us/?r=33ZsupGQr1rUmwj62HadcZ55hsGJajD9wN" title="Gains de Satoshis toutes les 180 minutes.">★ ★ ★ Onewayfaucet.us</option>
+ <option value="#" title="Scam à vérifier.">- Scam potentiel -</option>
+ <option value="https://bitminer.io/2345991" title="Gains de Satoshis permanent.&#013;Vérifier si le service transfert les Satoshis.">❄ ❄ ➙ Bitminer.io</option>
+ <option value="https://bitzonk.com?afid=3662" title="Gains de 500 Satoshis toutes les 30 minutes.&#013;Limite de 1500 Satoshis offerts.&#013;Plateforme de jeux qui nécessite un dépôt de Satoshis.">➘ ➘ ➘ Bitzonk.com</option>
+ <option value="https://btcprominer.life/228462" title="Gains de Satoshis permament.&#013;Vérifier si le service transfert les Satoshis.&#013;Des Satoshis sont transférés pour de nouvelles affiliations valides.">☢ ★ ➙ Btcprominer.life</option>
+ <option value="https://hashrev.com/?r=108695" title="Ajouter 40 affiliés pour recevoir 1 Bitcoin.">❄ ❄ ➙ Hashrev.com</option>
+ <option value="https://xtenfaucet.com/?r=34xh4B35bYqtWGUnxYeGTMJL7bwgrdw9Pp" title="Gains de Satoshis toutes les 30 minutes.&#013;Il faut ajouter son compte Bitcoin sur FaucetHub.&#013;Les transferts ne semblent pas se faire.">❄ ❄ ❄ Xtenfaucet.com</option>
+ <option value="#" title="Faire un don.">- Faire un don -</option>
+ <option value="https://www.visionduweb.eu/altcoins/bitcoins/qrcode/" title="Remercier le développeur.">Remercier le développeur</option>
 </select>
 </form>
 </span>
